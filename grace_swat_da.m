0abe94bc6c3ed7340d5cb7dc2ee6bf1686c9bc17
Kangning Huang <fytaso@gmail.com>
2013-03-28 10:19:59 +0800

    Delete the code for two-window assimilation

--- grace_swat_da.m
+++ grace_swat_da.m
@@ -18,85 +18,13 @@ matlabpool local 12
 %% Perturb initial model states
 perturbStd = 0.1;
 init_state_path = strcat(cd, '\initial_state_2003001\');
-fprintf('Start perturbing the initial states.');
+fprintf('Start perturbing the initial states.\n\n');
 perturbInitState(lst, perturbStd, init_state_path);
-fprintf('Perturbing finished.\n');
+fprintf('\nPerturbing finished.\n');
+fprintf('*****************************************************************\n\n\n');
 
 %% Main route
-enpars = cell(parcount,encount);
-enpars_a = cell(parcount,encount);
-
-'Run the first 2 months'
-runBegin = str2num(graceDate{1}(1:7));
-runEnd = str2num(graceDate{2}(9:15))+1;
-writeBegin = runBegin;
-writeEnd = runEnd-1;
-runSwat(lst, runBegin,runEnd, writeBegin,writeEnd);
-
-'Do data assimilation for first 2 months'
-% Read state
-for q = 3:encount+2
-    filename = cd;
-    filename = strcat(filename, '\Ensemble\', lst(q,1).name,'\');
-    [pars,mts] = readState2win(graceDate{1},graceDate{2},filename,hrupar);
-    for i = 1:parcount
-        enpars{i,q-2} = pars{i};
-    end
-end
-% Data assimilation
-for i = 1:parcount
-    n = length(pars{1});
-    xf_e = zeros(n, encount);
-    for q = 1:encount
-        xf_e(:,q) = enpars{i,q};
-    end
-    mit = mts{i};
-    y = graceData(i, 1:2)';
-    R = [obsstd 0; 0 obsstd];
-    xf_a = enkf(xf_e, mit, y, R);
-    for q = 1:encount
-        enpars{i,q} = xf_a;
-    end
-end
-
-for t = 1:size(graceDate,1)-2
-        
-    % Read model states of time window T1 and T2
-    t1 = graceDate(t+1,1);
-    t2 = graceDate(t+2,1);
-    for q = 3:encount+2
-        filename = cd;
-        filename = strcat(filename, '\Ensemble\', lst(q,1).name,'\');
-        [subs,mts] = readState2win(t1{1},t2{1},filename);
-        for i = 1:subcount
-            enpars{i,q-2} = subs{i};
-        end
-    end
-    
-    % Data Assimilation
-    for i = 1:subcount
-        n = size(subs{i},1);
-        xf_e = zeros(n, encount);
-        for q = 1:encount
-            xf_e(:,q) = enpars{i,q};
-        end
-        mit = mts{i};
-        y = graceData(i, t+1:t+2)';
-        R = [20 0; 0 20];
-        xf_a = enkf(xf_e, mit, y, R);
-        correctZeros = (xf_a >= 0);
-        xf_a = xf_a .* correctZeros;
-        for q = 1:encount
-            enpars{i,q} = xf_a;
-        end
-    end
-    
-    % Write model states of time window T1 and T2
-    for q = 3:encount+2
-        filename = cd;
-        filename = strcat(filename, '\Ensemble\', lst(q,1).name,'\');
-        writeState2win(subs, t1{1}, t2{1}, filename);
-    end
-    
-    strcat(t1{1}, ',' , t2{1}, ' finished.')
+fprintf('Start the data assimilation.\n');
+for t = 1:length(graceDate)
+    obsT = graceDate{t};
 end
